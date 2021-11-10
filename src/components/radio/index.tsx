5c4621aaa6be2d8416a26ab6749c0d42bb75cbe1
Sincenir <843398723@qq.com>
2021-11-10 17:17:48 +0800

    radio style update

--- src/components/radio/index.tsx
+++ src/components/radio/index.tsx
@@ -2,11 +2,18 @@ import React from "react";
 import { View, Text } from "@tarojs/components";
 import PropTypes from "prop-types";
 
-import { SiRadioProps } from "../../../types/radio";
-
+import { RadioOption, SiRadioProps } from "../../../types/radio";
 
 const SiRadio: React.FC<SiRadioProps> = (props) => {
-  const { value, options, valueKey, labelKey, disabledKey, disabled, onChange } = props;
+  const {
+    value,
+    options,
+    valueKey,
+    labelKey,
+    disabledKey,
+    disabled,
+    onChange,
+  } = props;
 
   const handleClick = (v: string, d: boolean) => {
     if (disabled) {
@@ -18,30 +25,57 @@ const SiRadio: React.FC<SiRadioProps> = (props) => {
     onChange(v);
   };
 
+  const isSelect = (v: any): boolean => {
+    return v === value;
+  };
+
+  const getIconClass = (v: RadioOption): string => {
+    const classes: string[] = ['option--icon']
+    if (isSelect(v[valueKey])) classes.push('option--icon__selected')
+    if (disabled) classes.push('option--icon__disable')
+    return classes.join(' ')
+  }
+
+  const getOptionClass = () => {
+    const classes: string[] = ['radio--option']
+    if (disabled) classes.push('radio--option__disabled')
+    return classes.join(' ')
+  }
+
   return (
-    <View>
+    <View className='radio__container'>
       {options.map((v, i) => {
         return (
           <View
-            className={`radio-info ${
-              disabled ? 'radio-info__disabled' : ''
-            }`}
+            className={getOptionClass()}
             key={v[valueKey] as string}
             onClick={() =>
               handleClick(v[valueKey] as string, v[disabledKey] as boolean)
             }
           >
-            <View
-              className={`radio-icon ${
-                v[valueKey] === value ? "radio-icon-selected" : ""
-              } ${
-                disabled ? 'radio-icon-disable' : ''
-              }`}
-            >
-              {v[valueKey] === value ? (<View className='radio-selected'></View>) : ''}
+            <View className='s-column' style={{ width: "100%" }}>
+              {/* default content */}
+              <View className='s-row'>
+                {/* icon */}
+                <View
+                  className={getIconClass(v)}
+                >
+                  {isSelect(v[valueKey]) ? (
+                    <View className='radio__selected'></View>
+                  ) : (
+                    ""
+                  )}
+                </View>
+                {/* text */}
+                <Text className='s-pr-md'>{v[labelKey] as string}</Text>
+              </View>
+              {/* slot content（show after selection） */}
+              {isSelect(v[valueKey]) ? (
+                <View className='s-ml-lg s-pl-sm'>{props.children(i, v)}</View>
+              ) : (
+                ""
+              )}
             </View>
-            <Text className='radio-text s-pr-md'>{v[labelKey] as string}</Text>
-            {props.children(i, v)}
           </View>
         );
       })}
@@ -49,18 +83,16 @@ const SiRadio: React.FC<SiRadioProps> = (props) => {
   );
 };
 
-
 SiRadio.defaultProps = {
-  value: '',
+  value: "",
   options: [],
-  labelKey: 'label',
-  valueKey: 'value',
-  disabledKey: 'disabled',
+  labelKey: "label",
+  valueKey: "value",
+  disabledKey: "disabled",
   disabled: false,
   onChange: () => void 0,
-  children: () => null
+  children: () => null,
 };
-
 
 SiRadio.propTypes = {
   value: PropTypes.any,
